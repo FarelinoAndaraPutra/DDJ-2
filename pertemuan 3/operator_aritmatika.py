d30dd5e637c0f26cbcd3a1318194d63f4813b0c0
FarelinoAndaraPutra <142191788+FarelinoAndaraPutra@users.noreply.github.com>
2024-01-29 09:31:45 +0700

    Add files via upload

---
+++ pertemuan 3/operator_aritmatika.py
@@ -0,0 +1,11 @@
+x = 5
+y = 3
+
+#penjumlaham
+print("penjumlahan 5 + 3 = ", x+y)
+print("pengurungan 5 - 3 = ", x-y)
+print("perkalian 5 * 3 = ", x*y)
+print("pembagian 5 / 3 = ", x/y)
+print("pembagian bulat 5 // 3 = ", x//y)
+print("Modulus 5 % 3 = ", x%y)
+print("perpangkatan 5 ** 3 = ", x**y)
